a338de18eb8638b2db8e5d158e3d15f6c9e8b71a
root <root@DESKTOP-IH5SEMH.localdomain>
2021-03-29 11:54:46 +0700

    Create space_counter function

---
+++ format.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int space_counter(str_arr[], int i)
+{
+
+        i++;
+}
+
+format_str(str_arr[])
+{
+    char check_arr[];
+    int i, j, k;                    /*i -  номер символа, который проверяем
+                                        j - кол-во символов в выходном массиве*/
+    space_counter(str_arr, i);      
+
+}
