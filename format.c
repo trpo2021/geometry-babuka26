173c73641d7444bb743b55228776b20521c8aa50
root <root@DESKTOP-IH5SEMH.localdomain>
2021-03-29 14:37:25 +0700

    Create check_str funсtion

--- format.c
+++ format.c
@@ -2,17 +2,36 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int space_counter(str_arr[], int i)
+int space_counter(char str_arr[], int i)
 {
-
+    while(str_arr[i] == ' ') {
         i++;
+    }
+    return i;
 }
 
-format_str(str_arr[])
+
+void check_str(char str_arr[],int i, int *j)
 {
-    char check_arr[];
-    int i, j, k;                    /*i -  номер символа, который проверяем
-                                        j - кол-во символов в выходном массиве*/
-    space_counter(str_arr, i);      
+    char check_arr[10];
+    int k = 0;
+    i = space_counter(str_arr, i);
+    while((str_arr[i] != '(') && (isalpha(str_arr[i]) > 0)) {
+        if(check_arr[k] != (tolower(str_arr[i]))) {
+            printf("Error: expected 'circle'");
+            exit(0);
+        }
+        i++;
+        j++;
+        k++;
+    }
+    
+}
+
+int format_str()
+{
+    int i = 0, j = 0;                    /*i -  номер символа, который проверяем
+                                    j - кол-во символов в выходном массиве*/
+    check_str(str_arr, i, &j);
 
 }
