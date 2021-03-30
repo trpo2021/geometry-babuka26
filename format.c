8006d0fad26d450be1c9a70e36cb85e5d1072e0c
root <root@DESKTOP-IH5SEMH.localdomain>
2021-03-30 12:16:49 +0700

    Create check_coordinates and check_radius functions

--- format.c
+++ format.c
@@ -11,9 +11,9 @@ int space_counter(char str_arr[], int i)
 }
 
 
-void check_str(char str_arr[],int i, int *j)
+int check_str(char str_arr[],int i, int *j)
 {
-    char check_arr[10];
+    char check_arr[] = {'c', 'i', 'r', 'c', 'l', 'e'};
     int k = 0;
     i = space_counter(str_arr, i);
     while((str_arr[i] != '(') && (isalpha(str_arr[i]) > 0)) {
@@ -25,13 +25,74 @@ void check_str(char str_arr[],int i, int *j)
         j++;
         k++;
     }
-    
+    return i;
+}
+
+int check_coordinates(char str_arr[], int i, int *j)
+{
+    int k = 0;
+
+    if(str_arr[i] == '-') {
+        i++;
+        k++;
+    }
+    if(isdigit(str_arr[i] == 0)) {
+        printf("Error: expected digit");
+        exit(0);
+    }
+    while((isdigit(str_arr[i] > 0)) || (str_arr[i] == '.')) {
+        if((k > 0) && (str_arr[i]) == '.') {
+            k = i;
+        }
+        if((str_arr[i] == '.') && (j > 0)) {
+            printf("Error: expected digit");
+            exit(0);
+        }
+    }
+    return i;
+}
+
+int check_radius(char str_arr[], int i, int *j)
+{
+    int k = 0;
+
+    if(str_arr[i] == '-') {
+        printf("Error: the radius cannot be negative");
+    }
+    if(isdigit(str_arr[i] == 0)) {
+        printf("Error: expected digit");
+        exit(0);
+    }
+    while((isdigit(str_arr[i] > 0)) || (str_arr[i] == '.')) {
+        if((k == 0) && (str_arr[i]) == '.') {
+            k = i;
+        }
+        k++;
+        i++;
+        if((str_arr[i] == '.') && (j > 0)) {
+            printf("Error: expected digit");
+            exit(0);
+        }
+    }
+    return i;
 }
 
 int format_str()
 {
     int i = 0, j = 0;                    /*i -  номер символа, который проверяем
                                     j - кол-во символов в выходном массиве*/
+    
+    int max_str = 100;
+    char str_arr[max_str];
+    FILE *file;
+
+    file = fopen("input.txt", "r");
+    fgets(str_arr, max_str, file);
+    
+    
     check_str(str_arr, i, &j);
+    check_coordinates(str_arr, i, &j);
+    check_radius(str_arr, i, &j);
+     
 
 }
